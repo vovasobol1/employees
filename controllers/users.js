6f8328afc10aae4debad408e72f7f525c9d1c743
vovasobol1 <dengyrostov@gmail.com>
2024-02-06 01:34:59 +0300

    feat: Добавлены функции для аутентификации пользователей
    Добавлены следующие функции:
    - `login`: обработка запроса на вход пользователя с проверкой электронной почты и пароля.
    - `register`: обработка запроса на регистрацию пользователя с хэшированием пароля и генерацией токена JWT.
    - `current`: обработка запроса на получение текущего пользователя (пустая функция, возможно, для будущего использования).
    
    Каждая функция имеет соответствующий маршрут, описание и доступ.
    
    Примечание:
    - Добавлены зависимости: `prisma`, `bcrypt`, `jsonwebtoken`.
    - Проверяется наличие обязательных полей в запросе.
    - Обработка ошибок в случае неверного логина или пароля, а также в случае, если пользователь с таким email уже существует.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,85 @@
+const {prisma} = require("../prisma/prisma-client");
+const brypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+//
+// @route POST /api/user/login
+// @desc логин
+// @acces Public
+//
+
 const login = async (req, res) => {
-    res.send('login');
+    const { email , password } = req.body
+
+    if (!email || !password ) {
+        return res.status(400).json({message : 'пожалуйста , заполните обязательные поля '})
+    }
+
+    const user = await prisma.user.findFirst({
+        where:{
+            email ,
+        }
+    })
+
+    const isPasswordCorrect = user &&  (await brypt.compare(password , user.password))
+
+    if (user && isPasswordCorrect){
+        res.status(200).json({
+            id : user.id ,
+            email : user.email ,
+            name : user.name
+        })
+    } else {
+        return res.status(400).json({ message : "неверно введен логин или пароль"})
+    }
+
 }
 
+
+// @route POST /api/user/register
+// @desc логин
+// @acces Public
 const register = async (req, res) => {
-    res.send('register');
+    const {email , password, name } = req.body;
+
+    if (!password || !email || !name){
+        return res.send(400).json({message: "заполните обязательные поля"})
+    }
+
+    const registeredUser = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    })
+
+    //если пользователь уже зареган
+    if (registeredUser){
+        return res.status(400).json({message: "пользователь с таким email уже существует"})
+    }
+
+    const salt = await brypt.genSalt(10);//строка которая добавляется к хешу
+    const hashedPassword = await brypt.hash(password , salt)
+
+    const user = await prisma.user.create({
+        data : {
+            email,
+            name ,
+            password : hashedPassword
+        }
+    })
+
+    const secret = process.env.JWT_SECRET ;
+
+    if (user && secret) {
+        res.status(201).json({
+            id : user.id ,
+            email : user.email,
+            name ,
+            token: jwt.sign({id : user.id} , secret , {expiresIn : "30d"})
+        })
+    }else {
+        return res.status(400).json({message: "не удалось создать пользователя"})
+    }
 }
 
 const current = async (req, res) => {

cfad3686d40c09fe9537e79186f4e187597fdd83
vovasobol1 <dengyrostov@gmail.com>
2024-04-16 15:43:37 +0300

    теперь при нажатии на кнопку логин можно попасть на главную страницу (если такой пользователь есть в базе данных)

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());

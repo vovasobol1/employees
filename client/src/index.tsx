1e62bb92ba7a307a978652dc7909a2aadf08ac75
vovasobol1 <dengyrostov@gmail.com>
2024-04-18 20:40:59 +0300

    добавлена страница редактирования сотрудника (пока не работает ничего)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -14,6 +14,7 @@ import {Employees} from "./pages/employees";
 import {AddEmployee} from "./pages/add-employee";
 import {Status} from "./pages/status";
 import {Employee} from "./pages/employee";
+import {EditEmployee} from "./pages/edit-employee";
 
 
 
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
     {
         path: `${Paths.employee}/:id`,
         element: <Employee/>
+    },
+    {
+        path: `${Paths.employeeEdit}/:id`,
+        element: <EditEmployee/>
     },
 ])
 
---
+++ client/src/pages/edit-employee/index.tsx
@@ -0,0 +1,54 @@
+import {useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEditEmployeeMutation, useGetEmployeeQuery} from "../../app/servises/employees";
+import {Row} from "antd";
+import {EmployeeForm} from "../../components/employee-form";
+import {Layout} from "../../components/layout";
+import {IsErrorWithMessage} from "../../utils/is-error-with-message";
+import {Employee} from "@prisma/client";
+
+
+export const EditEmployee = () => {
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+    const params = useParams<{ id: string }>()
+    const {data , isLoading} = useGetEmployeeQuery(params.id || "") // получает сотрудника по айди
+    const [editEmployee] = useEditEmployeeMutation()
+
+
+    const handleEditUser = async (employee : Employee) => {
+        try {
+            const editedEmployee = {
+                ...data ,
+                ...employee
+            }
+
+            await editEmployee(editedEmployee).unwrap()
+        }catch (err) {
+            const maybeError = IsErrorWithMessage(err)
+
+            if (maybeError){
+                setError(err.data.message)
+            }else {
+                setError('неизвестная ошибка')
+            }
+        }
+    }
+
+    if (isLoading){
+        return <span>загрузка</span>
+    }
+
+    return (
+        <Layout>
+            <Row align={'middle'} justify={'center'} >
+                <EmployeeForm employee={data}
+                              title={'Редактрировать'}
+                              btnTExt={'Подтвердить'}
+                              onFinish={handleEditUser}
+                              error={error}
+                />
+            </Row>
+        </Layout>
+    )
+}
